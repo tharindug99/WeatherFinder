3bb69f3c3cd1e2e2b24136d5d16ff05011392e7f
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2023-11-24 18:10:51 +0530

    add changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import reactIconsWi from 'react-icons/wi';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/WeatherFinder/"
+  base:"/WeatherFinder/",
+  build: {
+    rollupOptions: {
+      external: [
+        // ... other external dependencies
+        reactIconsWi,
+      ],
+    },
+  },
 })
